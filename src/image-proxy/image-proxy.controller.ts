2656af570414bfa55e450c4bf2b1d91f1d569124
Sam Bao <sam.b5687@gmail.com>
2024-02-17 15:45:58 -0700

    working image proxy

--- src/image-proxy/image-proxy.controller.ts
+++ src/image-proxy/image-proxy.controller.ts
@@ -1,20 +1,40 @@
-import { Controller, Get, Query,Res } from '@nestjs/common';
+import { Controller, Get, Query, Res, Req, HttpException, HttpStatus } from '@nestjs/common';
 import { Response } from 'express';
+import axios from 'axios';
+var url = require('url');
 
 @Controller('image-proxy')
 export class ImageProxyController {
 
- // This method will handle GET requests to /image-proxy
- @Get()
- getImage(@Res() res: Response, @Query('url') url: string, @Query('responseType') responseType: string) {
-    console.log("url: ");
-    console.log(url);
-    console.log("responseType:");
-    console.log(responseType);
+    // This method will handle GET requests to /image-proxy
+    @Get()
+    async getImage(@Res() res: Response, @Query('url') imageUrl: string, @Query('responseType') responseType: string) {
 
-    res.status(200).send(null);
+        // Validate the URL (similar to your validUrl middleware)
+        if (!imageUrl) {
+            throw new HttpException('No url specified', HttpStatus.BAD_REQUEST);
+        } else if (typeof imageUrl !== 'string' || !url.parse(imageUrl).host) {
+            throw new HttpException(`Invalid url specified: ${imageUrl}`, HttpStatus.BAD_REQUEST);
+        }
 
- }
+        try {
+            const response = await axios.get(imageUrl, { responseType: 'arraybuffer' });
+            const base64 = Buffer.from(response.data, 'binary').toString('base64');
+            const dataUrl = `data:${response.headers['content-type']};base64,${base64}`;
+      
+            switch (responseType) {
+              case 'blob':
+                res.send(response.data);
+                break;
+              case 'text':
+              default:
+                res.send(dataUrl);
+            }
+          } catch (error) {
+            console.error(error.message);
+            throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR);
+          }
+    }
 
 
 
